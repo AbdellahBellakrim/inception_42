c207251a47b682dec7f4a18b2980ef842605a0ee
abdellah <bellakrim2032@gmail.com>
2023-05-31 17:57:16 +0100

    mandatory is done but not perfect

--- files/srcs/requirements/wordpress/tool/wpconfig.sh
+++ files/srcs/requirements/wordpress/tool/wpconfig.sh
@@ -3,8 +3,6 @@
 mkdir -p /run/php
 sed -i 's/^listen = \/run\/php\/php7.3-fpm.sock/listen = wordpress:9000/' /etc/php/7.3/fpm/pool.d/www.conf
 
-cd /var/www/html && rm *
-
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 chmod +x wp-cli.phar
